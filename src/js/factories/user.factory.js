7af0f0a3abbb17a202aa222989dd38657cd06399
almikola <almikola@gmail.com>
2017-03-13 16:11:45 +0000

    Styling: main page

--- src/js/factories/user.factory.js
+++ src/js/factories/user.factory.js
@@ -5,7 +5,6 @@ angular
 userFactory.$inject = ['API', '$resource'];
 function userFactory(API, $resource){
   return $resource(`${API}/users/:id`, { id: '@_id'}, {
-    // emailSearch: { method: 'GET', url: `${API}/users/email/:email`},
     register: { method: 'POST', url: `${API}/register` },
     login: { method: 'POST', url: `${API}/login` },
     update: { method: 'PUT' }
